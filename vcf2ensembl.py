029982bc15d7ab9db49dce56f851018142fdc9a2
Adam David Scott <adam@adamscottphd.com>
2017-06-20 16:39:32 -0500

    now uses biomine for overlap cleaning

--- vcf2ensembl.py
+++ vcf2ensembl.py
@@ -1,6 +1,7 @@
 #!/bin/python
 #22 May 2017 - Adam Scott - 
  
+from biomine.variant import variant
 import sys
 import getopt
 
@@ -23,8 +24,8 @@ def buildEnsemblLine( chrom , start , stop , ref , alt , ID ):
 	ensFields.append( chrom )
 	ensFields.append( str( start ) )
 	ensFields.append( str( stop ) )
-	ref = nullCheck( ref )
-	alt = nullCheck( alt )
+	ref = variant.nullCheck( ref )
+	alt = variant.nullCheck( alt )
 	ensFields.append( ref + "/" + alt )
 	ensFields.append( "+" )
 	ensFields.append( ID )
@@ -33,45 +34,9 @@ def buildEnsemblLine( chrom , start , stop , ref , alt , ID ):
 def makeID( chrom , pos , ref , alt ):
 	return ':'.join( [ chrom , str( pos ) , ref , alt ] )
 
-def removeOverlap( pos , ref , alt ):
-	start = pos
-	stop = pos
-	updatedRef = ""
-	updatedAlt = ""
-	loverlap = 0
-	for i in range( 0 , min( [ len( ref ) , len( alt ) ] ) ):
-		if ref[i] == alt[i]:
-			loverlap = i
-			next
-		else:
-			break
-	if loverlap < len( ref ) - 1:
-		updatedRef = ref[loverlap+1:]
-	if loverlap < len( alt ) - 1:
-		updatedAlt = alt[loverlap+1:]
-	[ start , stop ] = getStartStop( pos , loverlap , updatedRef , updatedAlt )
-	updatedRef = nullCheck( updatedRef )
-	updatedAlt = nullCheck( updatedAlt )
-	return [ start , stop , updatedRef , updatedAlt ]
-
-def getStartStop( pos , loverlap , updatedRef , updatedAlt ):
-	start = pos + loverlap + 1
-	stop = pos
-	if len( updatedRef ) < len( updatedAlt ) and len( updatedRef ) == 0: #insertion
-		start -= 1
-		stop = start + 1
-	elif len( updatedRef ) > len( updatedAlt ) and len( updatedAlt ) == 0: #deletion
-		stop = start + len( updatedRef ) - 1
-	elif len( updatedRef ) > 0 and len( updatedAlt ) > 0: #complex
-		stop = start + len( updatedAlt )
-	else: #dunno
-		print( "Unusual ref alt start stop: " + updatedRef + " " + updatedAlt + " " + str( start ) + " " + str( stop ) )
-	return [ start , stop ]
-
-def nullCheck( val ):
-	if not val:
-		val = "-"
-	return val
+def removeOverlap( var ):
+	var.removeOverlapFromReferenceAndAlternate( )
+	return [ var.start , var.stop , var.reference , var.alternate ]
 
 def main( args ):
 	checkInputs( args )
@@ -93,16 +58,23 @@ def main( args ):
 			header += line
 			continue
 		fields = line.strip().split( "\t" )
+		print( fields )
 		chrom = str( fields[0] )
 		pos = int( fields[1] )
 		ref = fields[3].upper()
 		alt = fields[4].upper()
 		ensFields = [ str( chrom ) ]
+		var = variant(  chromosome = chrom , \
+						reference = ref , \
+						alternate = alt , \
+						start = pos , \
+						stop = pos \
+					)
 		ID = makeID( chrom , pos , ref , alt )
 		if len( ref ) == 1 and len( alt ) == 1: #is snv
 			ensFields = buildEnsemblLine( chrom , pos , pos , ref , alt , ID )
 		else:
-			[ start , stop , ref , alt ] = removeOverlap( pos , ref , alt )
+			[ start , stop , ref , alt ] = removeOverlap( var )
 			ensFields = buildEnsemblLine( chrom , start , stop , ref , alt , ID )
 		o.write( '\t'.join( ensFields ) )
 		o.write( "\n" )
